9fd1ee064b6fbac73a5b6116f389843ac2b83826
Damir Safarov <damup1988@gmail.com>
2021-05-09 22:22:09 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3rd try th create console application Tetris");
+            Console.ReadLine();
+        }
+    }
+}
