4f27adc857ce58012789f9e2649debdb90d38709
Damir Safarov <damup1988@gmail.com>
2021-05-13 22:41:52 +0300

    Line class has been added Borders have been added to Program class

---
+++ Line.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Line
+    {
+        public Point startPoint { get; set; }
+        public Direction direction { get; set; }
+        public int length { get; set; }
+
+        public Line(Point startPoint, Direction direction, int length)
+        {
+            this.startPoint = startPoint;
+            this.direction = direction;
+            this.length = length;
+        }
+
+        public void Draw()
+        {
+            if (direction == Direction.Right)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    startPoint.Draw('*');
+                    startPoint.x += 1;
+                }
+            }
+            if (direction == Direction.Down)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    startPoint.Draw('*');
+                    startPoint.y += 1;
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,17 @@ namespace Tetris
     {
         static void Main(string[] args)
         {
+            #region -=BORDERS=-
+            Line topLine = new Line(new Point(1, 1), Direction.Right, 35);
+            topLine.Draw();
+            Line leftLine = new Line(new Point(1, 1), Direction.Down, 25);
+            leftLine.Draw();
+            Line rightLine = new Line(new Point(36, 1), Direction.Down, 25);
+            rightLine.Draw();
+            Line bottomLine = new Line(new Point(1, 25), Direction.Right, 35);
+            bottomLine.Draw();
+            #endregion
+
             Point p1 = new Point(10, 5);
             p1.Draw('*');
 
