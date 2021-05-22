14de70fd55d18b026062064c23585fed706a89c5
Damir Safarov <damup1988@gmail.com>
2021-05-23 00:19:51 +0300

    Program.cs -> Added the mechanism to remove filled rows and score for it

--- Program.cs
+++ Program.cs
@@ -13,8 +13,9 @@ namespace Tetris
         {
             #region -=BORDERS=-
 
-            int playAreaHigh = 35;
-            int playAreaWidth = 35;
+            int playAreaHigh = 15;
+            int playAreaWidth = 15;
+            int row_length = playAreaWidth - 2;
 
             Line topLine = new Line(new Point(1, 1), Direction.Right, playAreaWidth);
             topLine.Draw();
@@ -27,18 +28,48 @@ namespace Tetris
             #endregion
 
             List<Point> currentBottom = new List<Point>();
-            
-            while (true)
+
+            int score = 0;
+            bool gameover = false;
+            while (!gameover)
             {
-                Point p1 = new Point(18, 2);
-                p1.Draw('*');
+                Point p1 = new Point(8, 2);
+                p1.Draw('*');                
 
                 while (!p1.hit_bottom_line & !p1.hit_currentBottom)
                 {
                     p1.Move(leftLine.startPoint.x, rightLine.startPoint.x, bottomLine.startPoint.y, currentBottom);
-                    Thread.Sleep(250);
+                    Thread.Sleep(250);                    
                 }                                
                 currentBottom.Add(p1);
+                var rows_numbers = currentBottom.Select(x => x.y).Distinct().ToList();
+                foreach (var uniq_row_number in rows_numbers)
+                {
+                    int col = 0;
+                    foreach (var cur_bottom_point in currentBottom)
+                    {
+                        if (cur_bottom_point.y == uniq_row_number)
+                        {
+                            col++;
+                        }
+                    }
+                    if (col == row_length)
+                    {
+                        foreach (var cur_bottom_point in currentBottom)
+                        {
+                            cur_bottom_point.Draw(' ');
+                        }
+                        currentBottom.RemoveAll(i => i.y == uniq_row_number);
+                        foreach (var cur_bottom_point in currentBottom)
+                        {
+                            cur_bottom_point.y += 1;
+                            cur_bottom_point.Draw('*');
+                        }
+                        score += 100;
+                        Console.SetCursorPosition(7, 20);
+                        Console.WriteLine($"SCORE: {score}");
+                    }
+                }
             }
 
             Console.SetCursorPosition(1, 26);
